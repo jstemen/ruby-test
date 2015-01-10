1561e1ca8baba32be820f066046348fd0a3f67ae
Jared Stemen <jstemen@gmail.com>
2015-01-09 18:20:40 -0800

    Problem 3  is close to working

--- helper.rb
+++ helper.rb
@@ -1,3 +1,33 @@
-class Helper
+require 'smarter_csv'
+module Helper
+  def self.go
 
+    file_str = './factual.input.txt'
+    File.readlines(file_str).each do |line|
+
+    end
+    clean_str = 'clean.txt'
+    File.open(clean_str, "w") do |out_line|
+      File.open(file_str, "r") do |in_line|
+        line = in_line.read
+        clean_line = line.gsub("\t\t\t", "\t").gsub("\t\t", "\t").gsub(/\r\n?/, "\n").gsub(/\t$/, "").gsub('"', '')
+        out_line.write(clean_line)
+      end
+    end
+    tracker_hash = {}
+    total_chunks = SmarterCSV.process(clean_str, {:chunk_size => 2, :col_sep => "\t", :row_sep => "\n"}) do |chunk|
+      chunk.each do |h| # you can post-process the data from each row to your heart's content, and also create virtual attributes:
+        h.each { |key, value|
+          tracker_hash[key.to_s] ||= [-1, nil]
+          in_length = value.to_s.chars.length
+          if in_length > tracker_hash[key.to_s].first
+            tracker_hash[key.to_s] = [in_length, h[:factual_id]]
+          end
+        }
+
+      end
+      #puts chunk.inspect # we could at this point pass the chunk to a Resque worker..
+    end
+    tracker_hash
+  end
 end
--- my_example_spec.rb
+++ my_example_spec.rb
@@ -1,22 +1,31 @@
-
 require 'json'
 require 'rspec'
 require_relative './neato_class'
+require_relative './helper'
 
 describe 'My behaviour' do
 
-  it 'convert string to hash' do
+  xit 'convert string to hash' do
     string = "{key:[[value_1, value_2],[value_3, value4]], 5:10:00AM]}"
-    expected_hash = {"key" => [["value_1", "value_2"],["value_3", "value4"]], 5=>"10:00AM"}
+    expected_hash = {"key" => [["value_1", "value_2"], ["value_3", "value4"]], 5 => "10:00AM"}
     out_put = eval string
     expect(out_put).eq expected_hash
   end
 
   it 'should do number 2' do
-    h = {"this" => [1,2,3,4,5,6], "that" => ['here', 'there', 'everywhere'], "other" => 'here'}
+    h = {"this" => [1, 2, 3, 4, 5, 6], "that" => ['here', 'there', 'everywhere'], "other" => 'here'}
     c = NeatoClass.new(h)
-    expect(c.this).to eq([1,2,3,4,5,6])
+    expect(c.this).to eq([1, 2, 3, 4, 5, 6])
     expect(c.that).to eq(['here', 'there', 'everywhere'])
     expect(c.other).to eq('here')
+  end
+
+  it 'should find the answer to 3' do
+    out = Helper.go
+    expected = {"factual_id" => [36, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "name" => [72, "900ba574-2cc5-4d22-bf2e-92fb14412abf"], "address" => [25, "9f56d3d6-f1e9-463c-b6b5-020bf2cd9492"], "address_extended" => [8, "59e079c4-cb58-4d0f-854a-671157dfcb55"], "po_box" => [13, "6ec565c0-2a8e-49ea-82a9-b905369185e9"], "locality" => [18, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "region" => [2, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "post_town" => [0, ""], "admin_region" => [0, ""], "postcode" => [5, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "country" => [2, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "tel" => [14, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "fax" => [14, "f9dfe097-74bf-469c-a2c8-901c4a58a80e"], "latitude" => [9, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "longitude" => [11, "59e079c4-cb58-4d0f-854a-671157dfcb55"], "neighborhood" => [57, "5598000e-23c4-4883-bc04-538f29ac07e6"], "website" => [41, "d65bf844-e07c-458d-8d27-10d7608bc79a"], "email" => [28, "a1db9414-01b4-4f7a-8cda-fbb7e007b04f"], "category_ids" => [7, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "category_labels" => [88, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "status" => [1, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "chain_name" => [0, ""], "chain_id" => [0, ""]}
+    expected.each{|key,value|
+      var = out[key]
+      expect(var).to eq(value), "key: #{key} : expected #{value} but got #{var}"
+    }
   end
 end
