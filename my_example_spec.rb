4be71f2483c39a84be6dd5929cad5da7e666f93a
Jared Stemen <jstemen@gmail.com>
2015-01-10 09:03:56 -0800

    formating

--- my_example_spec.rb
+++ my_example_spec.rb
@@ -28,8 +28,30 @@ describe 'My behaviour' do
   end
 
   it 'should find the answer to 3' do
+    expected = {"factual_id" => [36, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"],
+                "name" => [72, "900ba574-2cc5-4d22-bf2e-92fb14412abf"],
+                "address" => [25, "9f56d3d6-f1e9-463c-b6b5-020bf2cd9492"],
+                "address_extended" => [8, "59e079c4-cb58-4d0f-854a-671157dfcb55"],
+                "po_box" => [13, "6ec565c0-2a8e-49ea-82a9-b905369185e9"],
+                "locality" => [18, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"],
+                "region" => [2, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"],
+                "post_town" => [0, ""],
+                "admin_region" => [0, ""],
+                "postcode" => [5, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"],
+                "country" => [2, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"],
+                "tel" => [14, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"],
+                "fax" => [14, "f9dfe097-74bf-469c-a2c8-901c4a58a80e"],
+                "latitude" => [9, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"],
+                "longitude" => [11, "59e079c4-cb58-4d0f-854a-671157dfcb55"],
+                "neighborhood" => [57, "5598000e-23c4-4883-bc04-538f29ac07e6"],
+                "website" => [41, "d65bf844-e07c-458d-8d27-10d7608bc79a"],
+                "email" => [28, "a1db9414-01b4-4f7a-8cda-fbb7e007b04f"],
+                "category_ids" => [7, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"],
+                "category_labels" => [88, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"],
+                "status" => [1, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"],
+                "chain_name" => [0, ""],
+                "chain_id" => [0, ""]}
     out = Helper.go
-    expected = {"factual_id" => [36, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "name" => [72, "900ba574-2cc5-4d22-bf2e-92fb14412abf"], "address" => [25, "9f56d3d6-f1e9-463c-b6b5-020bf2cd9492"], "address_extended" => [8, "59e079c4-cb58-4d0f-854a-671157dfcb55"], "po_box" => [13, "6ec565c0-2a8e-49ea-82a9-b905369185e9"], "locality" => [18, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "region" => [2, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "post_town" => [0, ""], "admin_region" => [0, ""], "postcode" => [5, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "country" => [2, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "tel" => [14, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "fax" => [14, "f9dfe097-74bf-469c-a2c8-901c4a58a80e"], "latitude" => [9, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "longitude" => [11, "59e079c4-cb58-4d0f-854a-671157dfcb55"], "neighborhood" => [57, "5598000e-23c4-4883-bc04-538f29ac07e6"], "website" => [41, "d65bf844-e07c-458d-8d27-10d7608bc79a"], "email" => [28, "a1db9414-01b4-4f7a-8cda-fbb7e007b04f"], "category_ids" => [7, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "category_labels" => [88, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "status" => [1, "b716c681-ad67-4c9c-aab4-7f5fb39e3e11"], "chain_name" => [0, ""], "chain_id" => [0, ""]}
     expected.each{|key,value|
       var = out[key]
       expect(var).to eq(value), "key: #{key} : expected #{value} but got #{var}"
