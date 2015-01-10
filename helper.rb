879e626d7f5195bfc17f118d6a2bfbad8b5ba29a
Jared Stemen <jstemen@gmail.com>
2015-01-09 20:39:29 -0800

    simplifying

--- helper.rb
+++ helper.rb
@@ -3,30 +3,26 @@ module Helper
   def self.go
 
     file_str = './factual.input.txt'
-    File.readlines(file_str).each do |line|
 
-    end
-    clean_str = 'clean.txt'
-    File.open(clean_str, "w") do |out_line|
-      File.open(file_str, "r") do |in_line|
-        line = in_line.read
-        clean_line = line.gsub('"', '*')
-        out_line.write(clean_line)
-      end
-    end
     tracker_hash = {}
-    total_chunks = SmarterCSV.process(clean_str, {:remove_empty_values =>false, :strings_as_keys =>true, :chunk_size => 20, :col_sep => "\t", :row_sep => "\n"}) do |chunk|
-      chunk.each do |h| # you can post-process the data from each row to your heart's content, and also create virtual attributes:
-        h.each { |key, value|
+    opts_hash = {:force_simple_split => true,
+                 :strip_chars_from_headers => /[\-"]/,
+                 :remove_empty_values => false,
+                 :strings_as_keys => true,
+                 :chunk_size => 20,
+                 :col_sep => "\t",
+                 :row_sep => "\n"}
+    total_chunks = SmarterCSV.process(file_str, opts_hash) do |chunk|
+      chunk.each do |row|
+        row.each { |key, value|
           tracker_hash[key] ||= [0, ""]
           in_length = value.to_s.chars.length
           if in_length > tracker_hash[key].first
-            tracker_hash[key] = [in_length, h["factual_id"]]
+            tracker_hash[key] = [in_length, row["factual_id"]]
           end
         }
 
       end
-      #puts chunk.inspect # we could at this point pass the chunk to a Resque worker..
     end
     tracker_hash
   end
